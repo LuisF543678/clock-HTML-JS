ff036c6b8edd0de7c28f6dd762ee24e7c6136c99
luis_fernando <luis_fernando_mc@outlook.com>
2021-04-27 11:12:31 -0500

    starting tests

---
+++ clock.js
@@ -0,0 +1,36 @@
+/**
+ * Internet clock with data, better interface and new design
+ */
+const startingTime = function () {
+    var currentDate = new Date();
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+    timeSystem = (hours < 12) ? "<span>AM</span>" : "<span>PM</span>";
+    hours = (hours == 0) ? 12 : hours;
+    hours = (hours > 12) ? hours - 12 : hours;
+    //Add a zero in front of the numbers <10 
+    hours = checkTime(hours);
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+    document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds + " " + timeSystem;
+
+    var months = ['Enero', 'Fabrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var currentWeekDay = days[currentDate.getDay()];
+    var currentDay = currentDate.getDate();
+    var currentMonth = months[currentDate.getMonth()];
+    var currentYear = currentDate.getFullYear();
+    var date = currentWeekDay + ", " + currentDay + " " + currentMonth + " " + currentYear;
+    document.getElementById("date").innerHTML = currentDate;
+
+    var time = setTimeout(function () {
+        startingTime()
+    }, 500);
+}
+const checkTime = function (i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
